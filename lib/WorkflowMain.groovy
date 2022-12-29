552e567ce400bb4ea36269b30576cd3334530447
aldosr <aldo.sergi13@gmail.com>
2022-12-29 16:56:40 +0100

    Fixed error message on input check

--- lib/WorkflowMain.groovy
+++ lib/WorkflowMain.groovy
@@ -74,8 +74,7 @@ class WorkflowMain {
 
         // Check input has been provided
         if (!params.input_all && params.input_normal && params.input_tumor) {
-            log.error "Please provide an input samplesheet to the pipeline e.g. '--input samplesheet.csv'
-                        or an input BAM to be analyze"
+            log.error "Please provide an input samplesheet (or BAM) to the pipeline e.g. '--input samplesheet.csv'"
             System.exit(1)
         }
     }
